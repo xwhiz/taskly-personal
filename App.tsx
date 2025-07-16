ebb6f537c3a7b49e0ee0a2f9e3c3cb063b6660c7
Hamza <muhammadhamzam1486@gmail.com>
2025-07-16 18:32:40 +0500

    feat: create first item and add handler for it

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
+  function handleDeletePress() {
+    Alert.alert(
+      "Are you sure you want to delete this?",
+      "this action is irreversable!",
+      [
+        {
+          text: "Yes",
+          onPress: function () { console.log("Ok, deleting!"); }
+        },
+        {
+          text: "No",
+          onPress: function () { console.log("Cancelling!"); }
+        }
+      ]
+    );
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+        <TouchableOpacity activeOpacity={0.8} style={styles.button} onPress={handleDeletePress}>
+          <Text style={styles.buttonText}>DELETE</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +34,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.colorWhite,
     justifyContent: 'center',
   },
+  itemContainer: {
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    borderBottomWidth: 1,
+    borderColor: theme.colorCerulean,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
+  },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  }
 });
--- theme.ts
+++ theme.ts
@@ -1,3 +1,5 @@
 export const theme = {
-    colorCerulean: "#1a759f"
-}
+	colorCerulean: "#1a759f",
+	colorWhite: "#fff",
+	colorBlack: "#000",
+};
